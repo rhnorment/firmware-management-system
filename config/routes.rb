f179813eefd53b902cff94a4c406398108cb177c
Hunt Norment <hunt@sherpa.red>
2017-03-23 18:11:54 -0400

    accessible without api routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-  root to: 'admin/dashboard#index'
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  namespace :api, path: '/', constraints: { subdomain: 'api' } do
-    resources :firmware_builds, only: [:index, :show]
-  end
+  resources :firmware_builds, only: [:index, :show]
 
 end

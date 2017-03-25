374d77255782db72965c7618fa0eb72876f0f2ed
Hunt Norment <hunt@sherpa.red>
2017-03-25 17:35:42 -0400

    v0.5

--- app/admin/firmware_build.rb
+++ app/admin/firmware_build.rb
@@ -15,6 +15,8 @@ ActiveAdmin.register FirmwareBuild do
 
   menu priority: 2
 
+  config.sort_order = 'release_date_desc'
+
   permit_params :release_date,
                 :hardware_revision,
                 :software_revision,
@@ -33,7 +35,7 @@ ActiveAdmin.register FirmwareBuild do
   form do |f|
     f.semantic_errors *f.object.errors.keys
     f.inputs do
-      f.input :release_date, order: [:month, :day, :year], start_year: Date.today.year
+      f.input :release_date, as: :datepicker
       f.input :hardware_revision
       f.input :software_revision
       f.input :firmware_image, as: :file, hint: firmware_build.firmware_image.url

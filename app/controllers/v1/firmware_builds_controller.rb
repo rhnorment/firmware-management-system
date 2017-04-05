08ec0bf6da0749464e228039f614417e765d563a
Hunt Norment <hunt@sherpa.red>
2017-04-05 17:29:44 -0400

    remove cache on fw download

--- app/controllers/v1/firmware_builds_controller.rb
+++ app/controllers/v1/firmware_builds_controller.rb
@@ -18,15 +18,17 @@ module V1
     end
 
     def download_image
-      firmware_build = FirmwareBuild.latest_cached
+      firmware_build = FirmwareBuild.latest
 
       case params[:image]
       when 'image_a'
         filename = firmware_build.image_a.url.split('/').last
         send_file "#{Rails.root}/public/uploads/firmware_build/image_a/#{firmware_build.id}/#{filename}"
+        json_response(200)
       when 'image_b'
         filename = firmware_build.image_b.url.split('/').last
         send_file "#{Rails.root}/public/uploads/firmware_build/image_b/#{firmware_build.id}/#{filename}"
+        json_response(200)
       else
         json_response(404)
       end

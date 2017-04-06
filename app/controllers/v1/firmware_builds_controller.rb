62b63facf7c41fdaac6db7cccf338a5e528698bc
Hunt Norment <hunt@sherpa.red>
2017-04-06 16:59:38 -0400

    remove .env file

--- app/controllers/v1/firmware_builds_controller.rb
+++ app/controllers/v1/firmware_builds_controller.rb
@@ -16,23 +16,6 @@ module V1
       firmware_build = FirmwareBuild.find(params[:id])
       json_response(firmware_build)
     end
-
-    def download_image
-      firmware_build = FirmwareBuild.latest
-
-      case params[:image]
-      when 'image_a'
-        filename = firmware_build.image_a.url.split('/').last
-        send_file "#{Rails.root}/public/uploads/firmware_build/image_a/#{firmware_build.id}/#{filename}"
-        json_response(200)
-      when 'image_b'
-        filename = firmware_build.image_b.url.split('/').last
-        send_file "#{Rails.root}/public/uploads/firmware_build/image_b/#{firmware_build.id}/#{filename}"
-        json_response(200)
-      else
-        json_response(404)
-      end
-    end
   end
 
 end

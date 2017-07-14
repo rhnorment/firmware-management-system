038c2ea93ffa3ac755ab281d3395b391021dcaf5
Hunt Norment <hunt@sherpa.red>
2017-07-14 16:45:53 -0400

    email test

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings  = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDVRID_PASSWORD'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     address: 'smtp.sendgrid.net',
     port: 587,
